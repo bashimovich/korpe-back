7bd40df7a285af31bbef2c04f6364824ef00d0cf
bashimovich <yagshy.script@gmail.com>
2024-09-23 02:48:19 +0500

    latest of 22.09.24

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView
+from .views import BlogListView, BlogDetailView, BlogListThreeView
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='video-list'),
+    path('three', BlogListThreeView.as_view(), name='video-list-three'),
     path('<uuid:unique_key>/', BlogDetailView.as_view(), name='video-detail'),
 ]
